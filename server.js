6950ab080b73158f7cddf5a36530bbe0e7a6fb18
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-25 14:51:51 +0900

    特定のユーザーを取得する

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ app.get("/",(req,res)=>{
   res.send("hello")
 });
 
+//ユーザー情報を全て取得
 app.get("/users",(req,res)=>{
   pool.query("SELECT * FROM users",(error,results)=>{
     if(error) throw error;
     return res.status(200).json(results.rows);
-  })
-})
+  });
+});
+
+//特定のユーザーを取得する
+app.get("/users/:id",(req,res)=>{
+ const id = req.params.id
+  pool.query("SELECT * FROM users WHERE id=$1",[id],(error,results)=>{
+    if(error) throw error;
+    return res.status(200).json(results.rows);
+  });
+});
+
 
 app.listen(PORT,()=>{
   console.log("server is running on PORT"+PORT);
